78b25d346e3cf88d157e4d3b75b235ac32efc63e
bngold <bngoldyt@gmail.com>
2026-07-10 17:24:53 +0300

    add the new fornted deployment

--- src/environment/environment.prod.ts
+++ src/environment/environment.prod.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: true,
   apiUrl: 'https://dev-tracker-production-3ef3.up.railway.app',
   socketUrl: 'https://dev-tracker-production-3ef3.up.railway.app',
-  githubRedirectUri: 'https://strong-tartufo-f65dca.netlify.app/auth/github/callback'
+  githubRedirectUri: 'https://dev-tracker-api-five.vercel.app/auth/github/callback'
 };
